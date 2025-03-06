e003c4e67ef5acb55b4a4b7832c409c55426ffda
sujan khyaju <sujankhyaju2014@gmail.com>
2025-03-06 11:37:03 +0545

    added new files

---
+++ Multiply.py
@@ -0,0 +1,6 @@
+print("===============ADD=================")
+print("Please Enter two numbers:")
+a = int(input("Number 1:"))
+b = int(input("Number 2:"))
+result = a*b
+print(f"Sum of {a} and {b} is {result}")
